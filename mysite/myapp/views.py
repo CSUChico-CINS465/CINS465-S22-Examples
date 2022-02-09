91f7d153799e452be11a236a2e1bece332ba744d
Bryan Dixon <javawolfpack@gmail.com>
2022-02-09 08:13:59 -0800

    Basic Templates

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World")
+def index(request, page=0):
+    data_list = range(10*page,(page+1)*10,1)
+    context = {
+        "title": "CINS465",
+        "paragraph": "hello",
+        "data": data_list,
+        "page":page,
+        "next": page+1,
+        "prev": page-1
+    }
+    return render(request, "index.html", context=context)
