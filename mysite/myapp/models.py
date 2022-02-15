974935859c230d768c5489fb35cbf72e0f3ac135
Bryan Dixon <javawolfpack@gmail.com>
2022-02-15 14:48:09 -0800

    Models

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SuggestionModel(models.Model):
+    suggestion = models.CharField(max_length=240)
+
+    def __str__(self):
+        return "%s"%(
+            self.suggestion
+        )
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 def index(request, page=0):
-    data_list = range(10*page,(page+1)*10,1)
+    data_list = models.SuggestionModel.objects.all()
     context = {
         "title": "CINS465",
         "paragraph": "hello",
         "data": data_list,
-        "page":page,
-        "next": page+1,
-        "prev": page-1
     }
     return render(request, "index.html", context=context)
