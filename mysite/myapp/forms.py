8a0e52ddc71f8b3ca01689dea45f72833e1a7b0f
Bryan Dixon <javawolfpack@gmail.com>
2022-02-17 15:13:07 -0800

    Forms

---
+++ mysite/myapp/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.core.validators import validate_slug, validate_email
+
+from . import models
+
+def must_be_caps(value):
+    if not value.isupper():
+        raise forms.ValidationError("Not all upper case")
+    # Always return the cleaned data
+    return value
+
+def must_be_bob(value):
+    if not str(value).startswith("BOB"):
+        raise forms.ValidationError("Needs to start with BOB")
+    # Always return the cleaned data
+    return value
+
+def must_be_ten(value):
+    if not len(value) > 10:
+        raise forms.ValidationError("Needs to be more than 10 chars")
+    # Always return the cleaned data
+    return value
+
+class SuggestionForm(forms.Form):
+    suggestion_field = forms.CharField(
+        label='Suggestion',
+        max_length=240,
+        validators=[
+            validate_email
+            ]
+    )
+
+    def save(self):
+        suggestion_instance = models.SuggestionModel()
+        suggestion_instance.suggestion = self.cleaned_data["suggestion_field"]
+        suggestion_instance.save()
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from . import models
+from . import forms
 
 # Create your views here.
 def index(request, page=0):
+    if request.method == "POST":
+        form = forms.SuggestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = forms.SuggestionForm()
+    else:
+        form = forms.SuggestionForm()
     data_list = models.SuggestionModel.objects.all()
+
     context = {
         "title": "CINS465",
         "paragraph": "hello",
         "data": data_list,
+        "form": form,
     }
     return render(request, "index.html", context=context)
