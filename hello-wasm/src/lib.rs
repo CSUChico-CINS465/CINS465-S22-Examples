e558226c1b4532b555c7eb48a93282ac64a5f63f
Bryan Dixon <javawolfpack@gmail.com>
2022-04-14 21:59:41 +0000

    Broken Yew

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -1,4 +1,6 @@
 use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
 
 #[wasm_bindgen]
 extern {
@@ -9,3 +11,62 @@ extern {
 pub fn greet(name: &str) {
     alert(&format!("Hello World, {}!", name));
 }
+
+
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    // `ComponentLink` is like a reference to a component.
+    // It can be used to send messages to the component
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <h1>{{ "CINS465 Hello World" }}</h1>
+                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn run_app() {
+    yew::start_app::<Model>();
+}
+
+
